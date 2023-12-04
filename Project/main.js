b28eb68d30febb37bb28327ccbb906df2bb1066b
Mostercoder <84497752+Mostercoder@users.noreply.github.com>
2023-12-04 21:31:41 +0000

    css go brr

--- Project/main.js
+++ Project/main.js
@@ -2,5 +2,11 @@ function notification() {
     alert("Button clicked!");
 }
 
+function Animals() {
+    alert("you wanna buy some animals?");
+}
+
 // Attaching the function to the button click event
 document.getElementById("button_1").addEventListener("click", notification);
+
+document.getElementById("cat_1").addEventListener("click", Animals);
