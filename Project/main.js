3b1c4b7f6a95274ad875ee23b2f2ba2b2e4a79eb
Mostercoder <84497752+Mostercoder@users.noreply.github.com>
2023-12-04 17:21:59 +0000

    quality of life

--- Project/main.js
+++ Project/main.js
@@ -1,6 +1,6 @@
-function myFunction() {
+function notification() {
     alert("Button clicked!");
 }
 
 // Attaching the function to the button click event
-document.getElementById("button_1").addEventListener("click", myFunction);
+document.getElementById("button_1").addEventListener("click", notification);
