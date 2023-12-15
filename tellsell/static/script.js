ff219e714ef47308caf9316e4f4c3eaf3e549fad
Mostercoder <84497752+Mostercoder@users.noreply.github.com>
2023-12-15 23:05:59 +0000

    ez6

--- tellsell/static/script.js
+++ tellsell/static/script.js
@@ -25,16 +25,3 @@
 //}
 //
 //document.getElementById('addItemButton').addEventListener('click', addItem);
-
-function notification() {
-    alert("Button clicked!");
-}
-
-function Animals() {
-    alert("you wanna buy some animals?");
-}
-
-// Attaching the function to the button click event
-document.getElementById("button_1").addEventListener("click", notification);
-
-document.getElementById("cat_1").addEventListener("click", Animals);
